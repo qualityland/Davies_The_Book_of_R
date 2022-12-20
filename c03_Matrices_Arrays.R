0df39ae92d4925df2342e27bf0c36e237ab215d3
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2022-12-20 15:46:48 +0100

    chapter 3 started

---
+++ c03_Matrices_Arrays.R
@@ -0,0 +1,10 @@
+
+## 3.1 Defining a Matrix
+m1 <- matrix(data = c(1, 2, 3, 1967, 1969, 1973), nrow = 3, ncol = 2)
+m1
+
+
+## 3.1.1 Filling Direction
+
+m2 <- matrix(data = 1:6, nrow = 2, ncol = 3, byrow = FALSE)
+m2
